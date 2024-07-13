ea6daef1354623234147a263383e1acb92e2aac6
Adarsh Kumar <adarshff06@gmail.com>
2024-07-13 20:46:29 +0530

    include useEffect in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import Navbar from './components/Navbar'
 import { Outlet } from 'react-router-dom'
 import AuthenticationPage from './components/AuthenticationPage'
+import { useSelector , useDispatch} from 'react-redux'
+import authService from './AppWrite/Authentication'
+import { login, logout } from './components/store/Slice'
 
 function App() {
-  const [isLogin, setIslogin] = useState(true)
   
+  const [isAuthenticated , setIsAuthenticated] = useState(useSelector(state => state.isAuthenticated))
+  const [isLoading , setIsLoading] = useState(true)
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData){
+        useDispatch(login(userData))
+      } else {
+        useDispatch(logout())
+      }
+    })
+    .catch(error => console.log("App.jsx :: getCurrentUser :: error " , error))
+    setIsLoading(false)
+    console.log(isLoading)
+  } , [])
+
   return(
-    isLogin ? 
+    isAuthenticated ? 
       <div className='w-full h-screen bg-gray-400'>
       <Navbar />
       <Outlet />
